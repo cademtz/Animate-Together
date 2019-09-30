e6903759552a5e503c75957840122a9adad10af3
Hold on! <know.one.will.find.out@gmail.com>
2019-09-29 23:35:14 -0500

    Added x64 build and easy configurable deploy script for Windows

--- Deploy.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set /p SUBDIR=Deploy in subdir: 
-cd %SUBDIR%
-echo %cd%
-pause
-robocopy "C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86" "%cd%"
-C:\Qt\5.12.0\msvc2017\bin\windeployqt.exe
-pause
---
+++ WinDeploy.bat
@@ -0,0 +1,45 @@
+@echo off
+rem ===== Deploy settings =====
+set QT_DIR=C:\Qt
+set QT_VER=5.12.0
+set QT_CFG_32=msvc2017
+set QT_CFG_64=msvc2017_64
+set QT_OPT=--no-system-d3d-compiler --no-opengl-sw
+
+set VC_REDIST_32=C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x86
+set VC_REDIST_64=C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64
+
+rem ===== Initial settings =====
+set QT_CFG=%QT_CFG_32%
+set VC_REDIST=%VC_REDIST_32%
+
+rem ===== Input =====
+set /p SUBDIR=Deploy in subdir: 
+set /p CFG=Build config (x86 or x64): 
+
+if "%CFG%" neq "x86" if "CFG" neq "x64" goto bad_cfg
+if "%CFG%" neq "x64" goto deploy
+rem x64 was chosen:
+set VC_REDIST=%VC_REDIST_64%
+set QT_CFG=%QT_CFG_64%
+
+rem ===== Deploy =====
+:deploy
+cd %SUBDIR%
+echo Using directory: %cd%
+pause
+
+robocopy "%VC_REDIST%" "%cd%"
+copy LICENSE "%cd%\License.txt"
+
+echo %QT_DIR%\%QT_VER%\%QT_CFG%\bin\windeployqt.exe
+%QT_DIR%\%QT_VER%\%QT_CFG%\bin\windeployqt.exe %QT_OPT% "%cd%"
+
+pause
+exit
+
+rem ===== Error =====
+:bad_cfg
+echo "Bad configuration "%CFG", choose "x64" or "x86"
+pause
+exit
